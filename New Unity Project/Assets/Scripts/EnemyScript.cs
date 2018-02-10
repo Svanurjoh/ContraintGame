b7ec2e13fdaba995afd53b6c6a3d7212cf86bdfd
Svanurjoh <svanurjoh15@ru.is>
2018-02-10 18:34:00 +0000

    Moving things

--- New Unity Project/Assets/Scripts/EnemyScript.cs
+++ New Unity Project/Assets/Scripts/EnemyScript.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StarsScript : MonoBehaviour {
+public class EnemyScript : MonoBehaviour {
 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate (Vector3.down * 100 * Time.deltaTime);
 		float new_z = transform.position.z - 0.1f;
 		transform.position = new Vector3 (transform.position.x, transform.position.y, new_z);
 	}
---
+++ New Unity Project/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour 
+{
+	public GameObject astroidPrefab;
+	public GameObject starPrefab;
+	public GameObject enemyPrefab;
+
+	void Start () 
+	{
+		GameObject fireball = Instantiate(astroidPrefab, new Vector3(3,3,50), astroidPrefab.transform.rotation) as GameObject;
+		GameObject astroid = Instantiate(starPrefab, new Vector3(6,0,50), starPrefab.transform.rotation) as GameObject;
+		GameObject astroid1 = Instantiate(enemyPrefab, new Vector3(9,6,50), enemyPrefab.transform.rotation) as GameObject;
+	}
+
+	void Update () 
+	{
+		
+	}
+}
