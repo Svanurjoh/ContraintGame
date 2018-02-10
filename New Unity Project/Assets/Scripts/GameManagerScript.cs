75cb348f0a9870a76c89b3c19e19b8d8fb3dc636
Svanurjoh <svanurjoh15@ru.is>
2018-02-10 19:38:36 +0000

    random spawn

--- New Unity Project/Assets/Scripts/GameManagerScript.cs
+++ New Unity Project/Assets/Scripts/GameManagerScript.cs
@@ -8,15 +8,29 @@ public class GameManagerScript : MonoBehaviour
 	public GameObject starPrefab;
 	public GameObject enemyPrefab;
 
+	private float spawnTimer = 2.0f;
+
 	void Start () 
 	{
-		GameObject fireball = Instantiate(astroidPrefab, new Vector3(3,3,50), astroidPrefab.transform.rotation) as GameObject;
-		GameObject astroid = Instantiate(starPrefab, new Vector3(6,0,50), starPrefab.transform.rotation) as GameObject;
-		GameObject astroid1 = Instantiate(enemyPrefab, new Vector3(9,6,50), enemyPrefab.transform.rotation) as GameObject;
+		
 	}
 
 	void Update () 
 	{
-		
+		if (Time.time > spawnTimer) {
+			GameObject e = Instantiate(enemyPrefab, new Vector3(Random.Range(0, 14), Random.Range(0, 14),50), enemyPrefab.transform.rotation) as GameObject;
+			GameObject e1 = Instantiate(enemyPrefab, new Vector3(Random.Range(0, 14), Random.Range(0, 14),50), enemyPrefab.transform.rotation) as GameObject;
+			GameObject e2 = Instantiate(enemyPrefab, new Vector3(Random.Range(0, 14), Random.Range(0, 14),50), enemyPrefab.transform.rotation) as GameObject;
+			GameObject e3 = Instantiate(enemyPrefab, new Vector3(Random.Range(0, 14), Random.Range(0, 14),50), enemyPrefab.transform.rotation) as GameObject;
+
+			GameObject e8 = Instantiate(astroidPrefab, new Vector3(Random.Range(0, 14), Random.Range(0, 14),50), astroidPrefab.transform.rotation) as GameObject;
+			GameObject e7 = Instantiate(astroidPrefab, new Vector3(Random.Range(0, 14), Random.Range(0, 14),50), astroidPrefab.transform.rotation) as GameObject;
+			GameObject e6 = Instantiate(astroidPrefab, new Vector3(Random.Range(0, 14), Random.Range(0, 14),50), astroidPrefab.transform.rotation) as GameObject;
+			GameObject e5 = Instantiate(astroidPrefab, new Vector3(Random.Range(0, 14), Random.Range(0, 14),50), astroidPrefab.transform.rotation) as GameObject;
+
+			GameObject star = Instantiate(starPrefab, new Vector3(Random.Range(0, 14), Random.Range(0, 14),50), starPrefab.transform.rotation) as GameObject;
+
+			spawnTimer = Time.time + 2.0f;
+		}
 	}
 }
