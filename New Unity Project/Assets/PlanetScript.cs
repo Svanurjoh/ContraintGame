4e520ded1ec149e4a0867d30b53c54188340475a
gardarmj15 <gardarmj15@ru.is>
2018-02-11 14:38:14 +0000

    planet stuff

---
+++ New Unity Project/Assets/PlanetScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float new_z = transform.position.z - 0.01f;
+        if (transform.position.z > 40f)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, new_z);
+        }
+    }
+}
